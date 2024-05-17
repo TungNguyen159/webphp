e0604d2229c4f28e2fe746c28d91a56e8db6a352
Thanhtung159 <thanhtung150903@gmail.com>
2024-05-17 09:09:03 +0700

    commit15

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$database = "banhang";
-$username = "root";
-$password = "";
+$servername = "mysql8010.site4now.net";
+$username = "aa8d31_phonest";
+$password = "phone1509";
+$database = "db_aa8d31_phonest";
 
 // Create connection
 $conn =  mysqli_connect($servername ,$username, $password,$database);
